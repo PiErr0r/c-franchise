721cd50aa7699cb3f61a7aed8da6aa406227728a
Piero Anić <piero.anic@gmail.com>
2026-02-28 13:18:35 +0100

    + add some helpers

--- computational_geometry/main.c
+++ computational_geometry/main.c
@@ -13,6 +13,37 @@
 #define DIM 2
 typedef int tPointi[DIM];
 
+typedef struct tVertexStructure tsVertex;
+typedef tsVertex* tVertex;
+struct tVertexStructure {
+    int vnum;
+    tPointi v;
+    bool ear;
+    tVertex next, prev;
+};
+tVertex vertices = NULL;
+
+#define EXIT_FAILURE 1
+
+#define NEW(p, type)\
+    if ((p = (type *)malloc(sizeof(type))) == NULL) {\
+        printf("NEW: Out of memory");\
+        exit(EXIT_FAILURE);\
+    }
+
+#define ADD(head, p)\
+    if (head) {\
+        p->next = head;\
+        p->prev = head->prev;\
+        head->prev->next = p;\
+        head->prev = p;\
+    } else {\
+        head = p;\
+        head->next = head->prev = p;\
+    }
+
+#define FREE(p) if (p) { free((char*)p); p = NULL; }
+
 /** BEGIN: Visualization and logging helpers **/
 void print_tPointi(tPointi pt) {
     printf("{ .x = %d, .y = %d }\n", pt[X], pt[Y]);
@@ -44,6 +75,76 @@ double polygon_area(Vector2 *points, size_t n) {
         A += (points[i].x - points[i+1].x) * (points[i+1].y - points[i].y);
     }
     return A * 0.5f;
+}
+
+int Area2(tPointi a, tPointi b, tPointi c) {
+    return (b[X]-a[X])*(c[Y]-a[Y]) -
+           (b[Y]-a[Y])*(c[X]-a[X]);
+}
+
+int AreaPoly2(void) {
+    int sum = 0;
+    tVertex p, a;
+    p = vertices;
+    a = p->next;
+    do {
+        sum += Area2(p->v, a->v, a->next->v);
+        a = a->next;
+    } while (a->next != vertices);
+    return sum;
+}
+
+bool Left(tPointi a, tPointi b, tPointi c) {
+    return Area2(a, b, c) > 0;
+}
+
+bool LeftOn(tPointi a, tPointi b, tPointi c) {
+    return Area2(a, b, c) >= 0;
+}
+
+bool Collinear(tPointi a, tPointi b, tPointi c) {
+    return Area2(a, b, c) == 0;
+}
+
+bool Xor(bool a, bool b) {
+    // negate values to ensure 0/1
+    return !a ^ !b;
+}
+
+bool Between(tPointi a, tPointi b, tPointi c) {
+    if (!Collinear(a, b, c)) {
+        return false;
+    }
+    if (a[X] != b[X])
+        return a[X] <= c[X] && c[X] <= b[X] ||
+               a[X] >= c[X] && c[X] >= b[X];
+    else
+        return a[Y] <= c[Y] && c[Y] <= b[Y] ||
+               a[Y] >= c[Y] && c[Y] >= b[Y];
+}
+
+bool IntersectProp(tPointi a, tPointi b, tPointi c, tPointi d) {
+    if (Collinear(a, b, c) ||
+        Collinear(a, b, d) ||
+        Collinear(c, d, a) ||
+        Collinear(c, d, b)) {
+        return false;
+    }
+    return Xor(Left(a, b, c), Left(a, b, d)) &&
+           Xor(Left(c, d, a), Left(c, d, b));
+}
+
+bool Intersect(tPointi a, tPointi b, tPointi c, tPointi d) {
+    if        (   IntersectProp(a, b, c, d)) {
+        return true;
+    } else if (   Between(a, b, c)
+               || Between(a, b, d)
+               || Between(c, d, a)
+               || Between(c, d, b)
+              ) {
+        return true;
+    }
+    return false;
 }
 
 int main(void)
